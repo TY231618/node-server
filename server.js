aa0f362814beb0a902bfe032535fcbaa65a5180f
Tony <anthony.c.young18@gmail.com>
2018-03-20 13:57:08 +0000

    refactored server.js

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 const express = require('express');
-
 require('./services/passport');
 const app = express();
-// const authRoutes = require('./routes/authRoutes');
-
 const PORT = process.env.PORT || 5000;
 
-// authRoutes(app);
-// this require returns a function and we immediately call it with app
 require('./routes/authRoutes')(app);
 
 app.listen(PORT, (err) => {
 
-  if(err) return console.log('Error connecting to server: ', err);
+  if(err) {
+    return console.log('Error connecting to server: ', err);
+  }
 
   return console.log('Server started on port: ', PORT);
 });
