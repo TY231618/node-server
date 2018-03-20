636fcbd618318ef400fc3e5aa26a741309bd58c5
Tony <anthony.c.young18@gmail.com>
2018-03-20 10:26:28 +0000

    added engines object to package.json

--- api/server.js
+++ api/server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
-let port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send({hi: 'there'});
 });
 
-app.listen(port, (err) => {
+app.listen(PORT, (err) => {
 
-  if(err) return console.log('Error connecting to server');
+  if(err) return console.log('Error connecting to server: ', err);
 
-  return console.log('Server started on port: ', port);
+  return console.log('Server started on port: ', PORT);
 });
