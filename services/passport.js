3665517ecf659a3c7a6274649ffdc2b00671b443
Tony <anthony.c.young18@gmail.com>
2018-03-20 15:58:45 +0000

    adding user to db from google auth profile id and only saving once per id

--- services/passport.js
+++ services/passport.js
@@ -1,13 +1,28 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-  console.log('Access Token: ', accessToken);
-  console.log('Refresh Token: ', refreshToken);
-  console.log('Profile: ', profile);
+
+  User.findOne({ googleId: profile.id})
+    .then((existingUser) => {
+      if(existingUser) {
+        return console.log('User with that ID already exists');
+      }
+
+      new User({ googleId: profile.id}).save((err) => {
+        if(err) {
+          return console.log('Error saving User: ', err);
+        }
+    
+        return console.log('User saved successfully');
+      });
+    });
 }));
