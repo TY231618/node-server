a0175542fab9b7cc1de59126b2195bbef080d743
Tony <anthony.c.young18@gmail.com>
2018-03-20 14:57:18 +0000

    installed mongoose and hooked up db connection

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('./services/passport');
+const keys = require('./config/keys');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(keys.mongoURI, (err) => {
+  if(err) {
+    return console.log('MongoDB connection error: ', err);
+  }
+
+  return console.log('MongoDb connection successfull');
+});
 
 require('./routes/authRoutes')(app);
 
