8cf55226fd8f6a102ec171129defc7ff8e489126
Tony <anthony.c.young18@gmail.com>
2018-03-22 15:10:44 +0000

    refactored passport service to use asyn/await

--- services/passport.js
+++ services/passport.js
@@ -21,20 +21,21 @@ passport.use(new GoogleStrategy({
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback',
   proxy: true
-}, (accessToken, refreshToken, profile, done) => {
+}, async (accessToken, refreshToken, profile, done) => {
 
-  User.findOne({ googleId: profile.id})
-    .then((existingUser) => {
-      if(existingUser) {
-        console.log('User with that ID already exists');
-        done(null, existingUser);
-      } else {
-        new User({ googleId: profile.id, firstName: profile.name.givenName, lastName: profile.name.familyName, email: profile.emails[0].value})
-        .save()
-        .then(user => {
-          console.log('User created successfully');
-          done(null, user);
-        });
-      } 
-    });
+  const existingUser = await User.findOne({ googleId: profile.id});
+
+  if(existingUser) {
+    console.log('User with that ID already exists');
+    return done(null, existingUser);
+  } 
+  const user = await new User({ 
+    googleId: profile.id, 
+    firstName: profile.name.givenName, 
+    lastName: profile.name.familyName, 
+    email: profile.emails[0].value}).save();
+
+  console.log('User created successfully');
+  done(null, user);
+      
 }));
