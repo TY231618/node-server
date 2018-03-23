7df58492651d32f010655f8d2519002f62bf3397
Tony <anthony.c.young18@gmail.com>
2018-03-23 13:27:04 +0000

    wired up reducer

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
 
 import Header from './Header';
 
@@ -30,20 +32,26 @@ const Dashboard = () => {
   )
 };
 
-const App = () => {
+class App extends React.Component {
 
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path="/" component={ Landing } />
-          <Route exact path="/surveys" component={ Dashboard } />
-          <Route path="/surveys/new" component={ SurveyNew } />
-        </div>
-      </BrowserRouter>
-    </div>
-  )
+  componentDidMount() {
+    this.props.fetchUser();
+  };
+  
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={ Landing } />
+            <Route exact path="/surveys" component={ Dashboard } />
+            <Route path="/surveys/new" component={ SurveyNew } />
+          </div>
+        </BrowserRouter>
+      </div>
+    )
+  }
 };
 
-export default App;
+export default connect(null, { fetchUser: fetchUser })(App);
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,7 @@
 export default (state = {}, action) => {
 
+  console.log('actions: ', action);
+
   switch(action.type) {
     default:
       return state;
