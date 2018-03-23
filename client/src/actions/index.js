41aafa3e0a431739c5fe0f3d9e682e4186d4a27a
Tony <anthony.c.young18@gmail.com>
2018-03-23 13:52:45 +0000

    refactored fetchUser action to use async await

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,11 +1,19 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => {
+// export const fetchUser = () => {
 
-  return function(dispatch) {
-    axios
-      .get('/api/current_user')
-      .then((res) => dispatch({ type: FETCH_USER, payload: res }));
-  };
+//   return function(dispatch) {
+//     axios.get('/api/current_user')
+//       .then((res) => dispatch({ type: FETCH_USER, payload: res }));
+//   };
+
+// };
+
+//refactored to use asyn/await
+
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get('/api/current_user');
+
+  dispatch({ type: FETCH_USER, payload: res });
 };
