2c70f239e3cea9477e35c2e89b265385b254907a
Tony <anthony.c.young18@gmail.com>
2018-03-24 22:59:45 +0000

    made button better looking

--- client/src/components/payments.js
+++ client/src/components/payments.js
@@ -7,10 +7,14 @@ class Payments extends React.Component {
     
     return (
       <StripeCheckout
+        name="Email Survey"
+        description="$5 for 5 survey credits"
         amount={ 500 }
         token={ token => console.log('token: ', token) }
         stripeKey={ process.env.REACT_APP_STRIPE_KEY }
-      />
+      >
+        <button className="btn">Add Credits</button>
+      </StripeCheckout>
     );
   }
 }
