12eddafe4de84dec9b7ff33c410eaf3ad0e3693e
Tony <anthony.c.young18@gmail.com>
2018-03-26 16:47:09 +0100

    created Mailer and basic template for email content

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,9 +1,25 @@
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Survey = require('../models/Survey');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 module.exports = (app) => {
 
-  app.post('/api/surveys', requireLogin, (req, res) => {
+  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
 
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title: title,
+      subject: subject,
+      body: body, 
+      recipients: recipients.split(',').map(email => { return { email: email.trim() }; }),
+      // recipients: recipients.split(',').map(email => { email })
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
+
+    const mailer = new Mailer(survey, surveyTemplate(survey));
   });
 };
