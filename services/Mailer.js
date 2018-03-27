2a700904a55bda1243b0e90e40d1ad2c5a69520d
Tony <anthony.c.young18@gmail.com>
2018-03-27 10:37:27 +0100

    updated email template, updated route handler to remove credit, save survey, user and return user

--- services/Mailer.js
+++ services/Mailer.js
@@ -7,6 +7,8 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
 
+    this.sgApi = sendgrid(keys.sendGridKey);
+
     this.from_email = new helper.Email('no-reply@email-survey.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
@@ -39,6 +41,19 @@ class Mailer extends helper.Mail {
     });
 
     this.addPersonalization(personalize);
+  }
+
+  async send() {
+
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: this.toJSON()
+    });
+
+    const response = await this.sgApi.API(request);
+
+    return response;
   }
 }
 
