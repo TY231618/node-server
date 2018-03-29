e48cfe94a6ed3d0c3a3bfb513c5beb311ab06104
Tony <anthony.c.young18@gmail.com>
2018-03-29 11:37:46 +0100

    reversed survey list

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -6,7 +6,7 @@ class Dashboard extends React.Component {
 
   renderButton() {
     return (
-      <div className="fixed-action-btn">
+      <div className=" center">
         <Link to="/surveys/new" className="btn-floating btn-large red">
           <i className="large material-icons">add</i>
         </Link>
